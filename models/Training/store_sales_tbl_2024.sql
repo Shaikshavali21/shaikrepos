a925264e5489eb0e242ba37ec29b4576b64975f4
Divity Shaikshavali <shaik1995vali@gmail.com>
2024-05-16 11:28:51 +0000

    commit

---
+++ models/Training/store_sales_tbl_2024.sql
@@ -0,0 +1,3 @@
+with store_sales_tbl_2024 as(
+select SS_STORE_SK,SS_QUANTITY,SS_SALES_PRICE,SS_NET_PROFIT from prod.STORE_SALES_2024)
+select * from store_sales_tbl_2024
---
+++ models/Training/store_tbl_2024.sql
@@ -0,0 +1,3 @@
+with store_tbl_2024 as(
+select S_STORE_SK,S_STORE_ID,S_STORE_NAME,S_COUNTY from prod.STORE_2024)
+select * from store_tbl_2024
