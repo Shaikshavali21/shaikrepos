d130c1e7eb4ff13062d5a8926342cda0be00c92b
Divity Shaikshavali <shaik1995vali@gmail.com>
2024-05-17 06:35:10 +0000

    commit

--- models/Training/store_sales_tbl_2024.sql
+++ models/Training/store_sales_tbl_2024.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='table') }}
+
 with store_sales_tbl_2024 as(
 select SS_STORE_SK,SS_QUANTITY,SS_SALES_PRICE,SS_NET_PROFIT from prod.STORE_SALES_2024)
 select * from store_sales_tbl_2024
--- models/Training/store_tbl_2024.sql
+++ models/Training/store_tbl_2024.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='table') }}
+
 with store_tbl_2024 as(
 select S_STORE_SK,S_STORE_ID,S_STORE_NAME,S_COUNTY from prod.STORE_2024)
 select * from store_tbl_2024
