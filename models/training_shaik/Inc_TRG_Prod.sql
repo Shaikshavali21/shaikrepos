03e358b01f5ab67fb04545d1ffeb350224404338
Divity Shaikshavali <shaik1995vali@gmail.com>
2024-05-31 11:00:41 +0000

    commit

---
+++ models/training_shaik/Inc_TRG_Prod.sql
@@ -0,0 +1,8 @@
+{{config(materialized='incremental',
+          unique_key='prod_id')
+
+}}
+select * from Prod.SRC_Product
+{%if is_incremental()%}
+where updated_dt>(select max(updated_dt) from {{this}}
+{%endif%}
